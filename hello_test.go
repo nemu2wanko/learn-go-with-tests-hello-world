d80095e35ad8968c98149820cc0ebcec7dbdd275
Nobuchika Tanaka <sleepingdog.7799@gmail.com>
2022-10-03 04:26:45 +0900

    Add a language arg to Hello()

--- hello_test.go
+++ hello_test.go
@@ -9,17 +9,28 @@ func TestHello(t *testing.T) {
 			t.Errorf("got %q want %q", got, want)
 		}
 	}
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
 
+	t.Run("to a person", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+	t.Run("empty string", func(t *testing.T) {
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Lauren", "French")
+		want := "Bonjour, Lauren"
 		assertCorrectMessage(t, got, want)
 	})
 }
